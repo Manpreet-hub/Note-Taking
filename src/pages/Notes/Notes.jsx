17ab11ad875f7ecf2ae610058a16cf6e7eb26eca
Manpreet <manpreetisar@gmail.com>
2022-04-06 18:22:24 +0530

    refactor sidebar and text-editor

--- src/pages/Notes/Notes.jsx
+++ src/pages/Notes/Notes.jsx
@@ -1,37 +1,14 @@
 import { Header, Footer } from "../../Layouts/";
 import "./notes-page.css";
-import { Link } from "react-router-dom";
 import { TextEditor } from "../../components/TextEditor/TextEditor";
 import { NotesCard } from "../../components/NotesCard/NotesCard";
+import { Sidebar } from "./Sidebar";
 
 const Notes = () => {
   return (
     <>
       <Header />
-      <div className="sidebar">
-        <div className="sidebar-icon">
-          <div className="sidebar-icon-option">
-            <Link className="link" to="/notes">
-              Notes
-            </Link>
-          </div>
-          <div className="sidebar-icon-option">
-            <Link className="link" to="/archives">
-              Archives
-            </Link>
-          </div>
-          <div className="sidebar-icon-option">
-            <Link className="link" to="/labels">
-              Labels
-            </Link>
-          </div>
-          <div className="sidebar-icon-option">
-            <Link className="link" to="/trash">
-              Trash
-            </Link>
-          </div>
-        </div>
-      </div>
+      <Sidebar />
       <TextEditor />
       <NotesCard />
       <Footer />
